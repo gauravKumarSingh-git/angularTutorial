030982230601a7f1c23041fc3d18116ba48a034b
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-04-20 15:35:01 +0530

    assignment 2

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No Server was created!';
   serverName = 'TestServer';
+  username = '';
 
   constructor(){
     setTimeout(() => {
@@ -22,10 +23,19 @@ export class ServersComponent {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any){
     this.serverName = event.target.value;
+  }
+
+  onResetUsername(){
+    this.username = ''
+  }
+  
+  checkAllowReset(){
+    if(this.username.length > 0) return true;
+    return false;
   }
 }
