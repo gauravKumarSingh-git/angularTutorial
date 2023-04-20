677cb2cb97f96ac850fbc5b00305acde470dca7f
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-04-20 13:03:48 +0530

    event binding (click)

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -12,9 +12,15 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+
   constructor(){
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000)
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
   }
 }
