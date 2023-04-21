4bedf5c56e08caaa4b2d1aa3eeeb3fe5d2e3e2b6
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-04-21 15:31:52 +0530

    assignment3 completed

--- assignment3/src/app/app.component.ts
+++ assignment3/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment3';
+  display = true;
+  clicks = [];
+  // counter = 1;
+
+  onToggleDisplay(){
+    this.display = !this.display;
+    // this.clicks.push(this.counter++);
+    this.clicks.push(new Date());
+  }
+
 }
