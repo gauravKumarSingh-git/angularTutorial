55f5df16562deaa818c512a5c28e75fc20a2f163
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-05-08 12:52:50 +0530

    shopping app updated navigation added using events

--- shopping-app/src/app/app.component.ts
+++ shopping-app/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-app';
+
+  loadedFeature = 'recepies'
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
