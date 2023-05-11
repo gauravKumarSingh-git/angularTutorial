ae16e5933e7e3bc789d1e3ad6f54340e17b7462b
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-05-11 11:52:23 +0530

    custom directives

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight-directive';
+import { BetterHighlightComponent } from './better-highlight/better-highlight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    BasicHighlightDirective, BetterHighlightComponent
   ],
   imports: [
     BrowserModule,
