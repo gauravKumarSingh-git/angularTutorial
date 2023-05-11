0d0f2d78b33888cdefdd901994e685640ed1537b
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-05-12 00:02:51 +0530

    services complete

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,29 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { AccountsService } from '../accounts.service';
+import { LoggingService } from '../logging/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService, 
+              private accountService: AccountsService) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New Status ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    
+    this.accountService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+
+    // const service = new LoggingService();
+    // service.logStatusChange(accountStatus);
+    // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
