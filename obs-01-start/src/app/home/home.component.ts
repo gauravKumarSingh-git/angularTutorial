15eeba323f0dbde5a287e530ebe2ed02c7518135
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-06-25 14:35:38 +0530

    handling error

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -4,34 +4,38 @@ import { interval, Subscription, Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
   private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     // this.firstObsSubscription = interval(1000).subscribe(count => {
     //   console.log(count);
     // })
 
-    const customIntervalObservable = new Observable(
-      (observer) => {
-        let count = 0;
-        setInterval(() => {
-          observer.next(count);
-          count++;
-        }, 1000);
-      }
-    )
+    const customIntervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      }, 1000);
+    });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe((count)=> console.log(count));
+    this.firstObsSubscription = customIntervalObservable.subscribe((count) => {
+      console.log(count);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    });
   }
 
   ngOnDestroy(): void {
     this.firstObsSubscription.unsubscribe();
   }
-
 }
