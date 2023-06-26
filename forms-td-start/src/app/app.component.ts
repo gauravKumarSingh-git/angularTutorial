953401b53c3a9591d3917e3f333bda420279dbb6
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-06-26 16:24:59 +0530

    setValue and patchValue

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -14,6 +14,23 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+
+    // patch so that if some value already present in form it will not override it except the username here
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
+
   }
 
   // onSubmit(form: NgForm) {
