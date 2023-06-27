ec7b6de098b805bc56a9a8d8478c7296e66f8559
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-06-27 12:24:21 +0530

    reset forms

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -53,5 +53,7 @@ export class AppComponent {
     this.user.answer = this.signUpForm.value.questionAnswer;
     this.user.gender = this.signUpForm.value.gender;
     this.submitted = true;
+
+    this.signUpForm.reset();
   }
 }
