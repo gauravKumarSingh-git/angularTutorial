ea95b9ce230933a35c0da00c7a433e9c90b0459b
gauravKumarSingh-git <gauravkumarsingh.be18ele@pec.edu.in>
2023-07-04 15:45:03 +0530

    http started

---
+++ http-start/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'http-start';
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    console.log(postData);
+    this.http
+      .post(
+        'https://http-demo-b364c-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    this.fetchPosts();
+  }
+
+  onClearPosts() {}
+
+  private fetchPosts() {
+    this.http
+      .get('https://http-demo-b364c-default-rtdb.firebaseio.com/posts.json')
+      .subscribe((posts) => console.log(posts));
+  }
+}
